764da2e2a3852c521dc9e418c1865905d286f755
0x67 <evilisnj@gmail.com>
2023-05-30 17:52:57 +0800

    Initial Commit

---
+++ 75646856/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  75646856
+//
+//  Created by 0x67 on 2023-05-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
